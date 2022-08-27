5e9825c57dfef757be4e5a2840f298533016b39e
ELENA <battilena@gmail.com>
2022-08-27 18:56:09 +0300

    task12

---
+++ homework_task12/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу,которая на вход принимает цифру, обозначающую день недели
+// и проверяет является ли этот день выходным
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n <= 5) Console.WriteLine("нет");
+
+else 
+{
+    Console.WriteLine("да, сегодня выходной");
+}
