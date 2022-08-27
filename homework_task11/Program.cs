dded157146e1d00907356912b141f44b8eb91107
ELENA <battilena@gmail.com>
2022-08-27 18:14:09 +0300

    Task_11

---
+++ homework_task11/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if( n <= 99) Console.Write("третей цифры нет");
+else{
+
+while
+(n > 999)
+{
+    n = n / 10;
+}
+   Console.Write($"{n % 10 } ");
+}
